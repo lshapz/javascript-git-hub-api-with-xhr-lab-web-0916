c567942cda570a4e5caceaf3690b236c6f985c74
lshapz <laurashapiro01@gmail.com>
2016-11-10 10:56:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const rootURL = "https://api.github.com"
 
 function getRepositories() {
-  var username = $('input#username').value
+  var username = document.getElementById("username").value
   const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories);
   const uri = rootURL + "/users/" + username + "/repos"
   req.addEventListener("load", displayRepositories)
-
   req.open("GET", uri)
   req.send()
   return false;
 }
 
+function displayRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = "<ul>" + repos.map(r => {
+    const dataUser = 'data-username="' + r.owner.login + '"';
+    const dataRepo = 'data-repository="' + r.name + '"';
+    return (`
+      <li>
+      <h2>${r.name}</h2>
+      <a href="${r.html_url}">${r.html_url}</a><br>
+      <a href="#" ${dataRepo} ${dataUser} onclick="getCommits(this)">Get Commits</a><br>
+      <a href="#" ${dataRepo} ${dataUser} onclick="getBranches(this)">Get Branches</a></li>
+
+      </li> `)}).join('') + "</ul>"
+    document.getElementById("repositories").innerHTML = repoList
+
+}
+
 function getCommits(el) {
-  const name = el.dataset.repo
-    const uri = rootURL + "/repos/" + el.dataset.username + "/" +repoName + "/commits"
+  const name = el.dataset.repository
+    const uri = rootURL + "/repos/" + el.dataset.username + "/" + name + "/commits"
 
   const req = new XMLHttpRequest()
   req.addEventListener("load", displayCommits);
@@ -30,23 +46,6 @@ function displayCommits() {
 }
 
 
-function displayRepositories(event, data) {
-  var repos = JSON.parse(this.responseText)
-  console.log(repos)
-  const repoList = "<ul>" repos.map(r => {
-    const dataUser = 'data-username="' + r.owner.login + '"';
-    const dataRepo = 'data-repository="' + r.name + '"';
-    return (`
-      <li>
-      <h2>${r.name}</h2>
-      <a href="${r.html_url}">${r.html_url}</a><br>
-      <a href="#" ${dataRepo} ${dataUser} onclick="getCommits(this)">Get Commits</a><br>
-      <a href="#" ${dataRepo} ${dataUser} onclick="getBranches(this)">Get Branches</a></li>
-
-      </li> `)}).join('') + "</ul>"
-    document.getElementById("repositories").innerHTML = repoList
-
-}
 
 function getBranches(el) {
   const repoName = el.dataset.repository
